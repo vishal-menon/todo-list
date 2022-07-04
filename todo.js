ab3de5d7cdc2cf87b6055758aa15f5fc3f5e3baf
vishal-menon <vishaljmenon@gmail.com>
2022-07-04 17:30:11 +0530

    First Commit
    Completed most functionality - need to fix checkboxes

---
+++ todo.js
@@ -0,0 +1,189 @@
+//Creating the Panel Div
+let newPanel = (element) => {
+
+let panel = document.createElement("div");
+panel.setAttribute("class","panel");
+panel.setAttribute("id",`${element.uid}`);
+
+let checkboxDiv = document.createElement("div");
+checkboxDiv.setAttribute("class","checkbox-div");
+panel.appendChild(checkboxDiv);
+
+let checkbox = document.createElement("input");
+checkbox.setAttribute("class", "task-checkbox");
+checkbox.setAttribute("type", "checkbox");
+checkboxDiv.appendChild(checkbox);
+
+let taskTextboxDiv = document.createElement("div");
+taskTextboxDiv.setAttribute("class","task-textbox-div");
+panel.appendChild(taskTextboxDiv);
+
+let taskText = document.createElement("input");
+taskText.setAttribute("class","task-text");
+taskText.setAttribute("type","text");
+taskText.setAttribute("disabled",true);
+taskText.setAttribute("id",`t${element.uid}`);
+taskText.value = element.taskTextValue;
+taskTextboxDiv.appendChild(taskText);
+
+let buttonsDiv = document.createElement("div");
+buttonsDiv.setAttribute("class","buttons-div");
+panel.appendChild(buttonsDiv);
+
+let editButton = document.createElement("button");
+editButton.setAttribute("class","edit-button");
+editButton.setAttribute("id",`${element.uid}`);
+editButton.innerText = 'Edit';
+editButton.onclick = editTodo;
+buttonsDiv.appendChild(editButton);
+
+let deleteButton = document.createElement("button");
+deleteButton.setAttribute("class","delete-button");
+deleteButton.innerText = 'Delete';
+deleteButton.setAttribute("id",`${element.uid}`);
+deleteButton.onclick = deleteTodo;
+buttonsDiv.appendChild(deleteButton);
+
+return panel;
+
+}
+
+//Model
+let listParent = document.getElementById('white-list');
+let panelItem = document.getElementById('panel-item');
+
+let submitButton = document.getElementById('submit');
+let inputText = document.getElementById('input-text');
+
+let arrayTasks = [];
+
+let pushToArray = (inputTextp, id) => {
+  arrayTasks.push(
+    {
+    isComplete : false,
+    taskTextValue : inputTextp.value,
+    uid : id
+    }
+  );
+}
+
+let deleteFromArray = (uid) => {
+  arrayTasks = arrayTasks.filter( element => {
+
+    if(element.uid === +uid)
+      return false;
+    else
+      return true;
+
+  });
+}
+
+let editArray = (uid, changedText) => {
+  
+  arrayTasks.forEach(element => {
+
+    if(element.uid === +uid)
+    {
+        element.taskTextValue = changedText;
+    }
+
+  });
+
+}
+
+/*
+  {
+    isComplete : bool,
+    taskTextValue : string
+  }
+*/
+
+//View
+
+let render = () => {
+
+  //Initially delete all content
+  listParent.innerHTML = '';
+
+  arrayTasks.forEach(element => {
+
+    listParent.appendChild(newPanel(element));
+
+  });
+
+}
+
+let editGraphics = (editTaskTextbox, button) => {
+
+  editTaskTextbox.style.backgroundColor = "#f0f2f0";
+  editTaskTextbox.focus();
+  button.innerText = "Save";
+
+}
+
+
+//Controller
+
+submitButton.onclick = () => 
+{
+  //give the text and id to insert to the array
+
+  const id = new Date().getTime();
+
+  pushToArray(inputText, id);
+
+  //give the array to display to render
+  render();
+
+  console.log(arrayTasks);
+}
+
+deleteTodo = (event) => {
+
+  console.log(event);
+  
+  const uid = event.target.id;
+
+  deleteFromArray(uid);
+
+  render();
+
+}
+
+editTodo = (event) => {
+
+  console.log(event);
+
+  const uid = event.target.id;
+
+  let button = event.target;
+
+  let editTaskTextbox = document.getElementById(`t${uid}`);
+  editTaskTextbox.removeAttribute("disabled");
+
+  editGraphics(editTaskTextbox, button);
+
+  event.target.onclick = saveTodo;
+}
+
+saveTodo = (event) => {
+
+  const uid = event.target.id;
+
+  let editTaskTextbox = document.getElementById(`t${uid}`);
+  editTaskTextbox.setAttribute("disabled",true);
+
+  editArray(uid, editTaskTextbox.value);
+
+  event.target.onclick = editTodo;
+
+  render();
+
+}
+
+
+
+
+
+
+
